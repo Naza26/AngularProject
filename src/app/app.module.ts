a8ac10d1ccadb0c790cc6a748df76a860c14a9cd
Nazarena Rueda <naza120@gmail.com>
2022-07-23 13:18:26 -0300

    adding main sections of the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { MainComponentComponent } from './main-component/main-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponentComponent,
+    HeaderComponentComponent,
+    MainComponentComponent
   ],
   imports: [
     BrowserModule,
