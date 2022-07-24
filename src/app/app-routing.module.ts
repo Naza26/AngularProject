261d5996b75d5c4d0c84ca35b7768d3d7e8dd111
Nazarena Rueda <naza120@gmail.com>
2022-07-24 18:39:57 -0300

    adding routing for accounts section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './account/account.component';
 import { AddMoviesComponent } from './movies/add-movies/add-movies.component';
 import { DeleteMoviesComponent } from './movies/delete-movies/delete-movies.component';
 import { EditMoviesComponent } from './movies/edit-movies/edit-movies.component';
 import { MoviesComponent } from './movies/movies.component';
+import { NewAccountComponent } from './new-account/new-account.component';
 
 const routes: Routes = [
   { path: '', component: MoviesComponent },
   { path: 'addMovie', component: AddMoviesComponent },
   { path: 'editMovie', component: EditMoviesComponent },
-  { path: 'deleteMovie', component: DeleteMoviesComponent }
+  { path: 'deleteMovie', component: DeleteMoviesComponent },
+  { path: 'accounts', component: AccountComponent },
+  { path: 'addAccount', component: NewAccountComponent },
 ];
 
 @NgModule({
