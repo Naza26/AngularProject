1cdfd6107826efac6487c009b062e5401c44c2bb
Nazarena Rueda <naza120@gmail.com>
2022-07-24 18:31:14 -0300

    displaying fetched data from firebase

--- src/app/movies/edit-movies/edit-movies.component.ts
+++ src/app/movies/edit-movies/edit-movies.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-movies',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-movies.component.css']
 })
 export class EditMoviesComponent implements OnInit {
+  loadedData: any = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  onFetchMovieData() {
+    this.onFetchMovies();
+  }
+
+  private onFetchMovies() {
+    // Get Http Request
+    this.http.get('https://sample-project-bf329-default-rtdb.firebaseio.com/posts.json')
+    .pipe(map(responseData => {
+      const postArray = [];
+      for (const key in responseData) {
+        if (responseData.hasOwnProperty(key)) {
+          postArray.push({...responseData[key as keyof Object], id: key});
+        }
+      }
+      return postArray;
+    }))
+    .subscribe(posts => {
+      this.loadedData = posts;
+    });
   }
 
 }
