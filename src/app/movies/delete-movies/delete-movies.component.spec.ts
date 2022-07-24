d1f47cdd5b1ec439960874be94a981366372af74
Nazarena Rueda <naza120@gmail.com>
2022-07-24 00:19:26 -0300

    adding delete-movie section

---
+++ src/app/movies/delete-movies/delete-movies.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DeleteMoviesComponent } from './delete-movies.component';
+
+describe('DeleteMoviesComponent', () => {
+  let component: DeleteMoviesComponent;
+  let fixture: ComponentFixture<DeleteMoviesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DeleteMoviesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DeleteMoviesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
