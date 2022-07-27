96fcdcd2cd7cff16267645f649bdf7be83e89893
Nazarena Rueda <naza120@gmail.com>
2022-07-27 18:14:20 -0300

    adapting model properties

--- src/app/models/movie.model.ts
+++ src/app/models/movie.model.ts
@@ -2,12 +2,14 @@ export class Movie {
     public title: string;
     public releaseYear: string;
     public description: string;
-    public imagePath: string;
+    public seenMovie: boolean;
+    public imagePath?: string;
 
-    constructor(title: string, releaseDate: string, description: string, image: string) {
+    constructor(title: string, releaseDate: string, description: string, seenMovie: boolean, image: string) {
         this.title = title;
         this.releaseYear = releaseDate;
         this.description = description;
+        this.seenMovie = seenMovie;
         this.imagePath = image;
     }
 }
