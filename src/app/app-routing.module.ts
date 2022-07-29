16d5d2b6e23b9b44374e74e4b03d73c3d4050119
Nazarena Rueda <naza120@gmail.com>
2022-07-29 13:03:52 -0300

    deleting unused components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AccountComponent } from './account/account.component';
 import { AuthComponent } from './auth/auth.component';
 import { AddMoviesComponent } from './movies/add-movies/add-movies.component';
 import { DeleteMoviesComponent } from './movies/delete-movies/delete-movies.component';
 import { EditMoviesComponent } from './movies/edit-movies/edit-movies.component';
 import { MoviesComponent } from './movies/movies.component';
-import { NewAccountComponent } from './new-account/new-account.component';
 
 const routes: Routes = [
   { path: '', component: MoviesComponent },
   { path: 'addMovie', component: AddMoviesComponent },
   { path: 'editMovie', component: EditMoviesComponent },
   { path: 'deleteMovie', component: DeleteMoviesComponent },
-  { path: 'accounts', component: AccountComponent },
-  { path: 'addAccount', component: NewAccountComponent },
   { path: 'auth', component: AuthComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { NewAccountComponent } from './new-account/new-account.component';
 import { MoviesComponent } from './movies/movies.component';
 import { MoviesDetailComponent } from './movies/movies-detail/movies-detail.component';
 import { FormsModule } from '@angular/forms';
 import { AddMoviesComponent } from './movies/add-movies/add-movies.component';
 import { EditMoviesComponent } from './movies/edit-movies/edit-movies.component';
 import { DeleteMoviesComponent } from './movies/delete-movies/delete-movies.component';
-import { AccountComponent } from './account/account.component';
 import { AccountService } from './account.service';
 import { LoggingService } from './logging.service';
 import { HttpClientModule } from '@angular/common/http';
@@ -24,14 +22,11 @@ import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.compo
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    NewAccountComponent,
-    NewAccountComponent,
     MoviesComponent,
     MoviesDetailComponent,
     AddMoviesComponent,
     EditMoviesComponent,
     DeleteMoviesComponent,
-    AccountComponent,
     AuthComponent,
     LoadingSpinnerComponent
   ],
